c0bf33b51f8f3076e8a0ec0b07d9f3677e070920
ygl <lanyueos@qq.com>
2019-07-24 19:40:16 +0800

    buid docs

--- docs/static/js/0.5bea6c0f.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0],{227:function(e,n,t){"use strict";function u(){return r.a.createElement("div",null,"Admin Page3")}Object.defineProperty(n,"__esModule",{value:!0}),n.default=u;var a=t(0),r=t.n(a)}});
-//# sourceMappingURL=0.5bea6c0f.chunk.js.map
---
+++ docs/static/js/0.d20f7d51.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],{228:function(e,n,t){"use strict";function u(){return r.a.createElement("div",null,"Admin Page3")}Object.defineProperty(n,"__esModule",{value:!0}),n.default=u;var a=t(0),r=t.n(a)}});
+//# sourceMappingURL=0.d20f7d51.chunk.js.map
